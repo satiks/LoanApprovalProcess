312b92b10cd533f95804a35d714bdecc8778f958
sande <sander.tiks@gmail.com>
2026-04-19 22:14:55 +0300

    fix(exception): add package declaration to ResourceNotFoundException

--- src/main/java/com/satiks/loanapp/exception/ResourceNotFoundException.java
+++ src/main/java/com/satiks/loanapp/exception/ResourceNotFoundException.java
@@ -1,3 +1,5 @@
+package com.satiks.loanapp.exception;
+
 /**
  * Thrown when a requested resource (entity, document, etc.) cannot be found in the system.
  * Typically results in a 404 HTTP response.
