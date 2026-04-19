2c600b95720a30ebfa7f278e3eb59b8d218dd73f
sande <sander.tiks@gmail.com>
2026-04-19 13:52:07 +0300

    feat: initialize Spring Boot loan application project

---
+++ src/main/java/com/satiks/loanapp/LoanappApplication.java
@@ -0,0 +1,19 @@
+package com.satiks.loanapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class LoanappApplication {
+
+	/**
+	 * Starts the Spring Boot application.
+	 *
+	 * @param args command-line arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(LoanappApplication.class, args);
+	}
+
+}
